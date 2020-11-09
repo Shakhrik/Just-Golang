23156df1f87c11f7f2b04381358fcaeed7f8cc63
Shakhriyor <ismatov.shaxriyor@mail.ru>
2020-11-09 17:11:02 +0500

    Added contact_test.go file

---
+++ struct/Contactlist/contact_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+var all = NewAllContacts()
+
+func TestAddingNewContact(t *testing.T) {
+	c := &Contact{"Shakhriyor", "Ismatov", "998933559909", 1}
+	saved := all.AddContact(c)
+	if saved.id != c.id {
+		t.Error("Error! Can't be added")
+	}
+
+}
+func TestUpdateContact(t *testing.T) {
+	all.contacts[0] = Contact{"Barello", "Tomas", "53443344", 0}
+	s := Contact{"Jon", "Boris", "949595494", 0}
+	update := all.UpdateContact(&s)
+	if update == &s {
+		t.Error("Error! Can't be updated", update, s)
+	}
+
+}
+func TestGetAllContacts(t *testing.T) {
+	if len(all.contacts) != len(all.getAllcontacts()) {
+		t.Error("Error! Not same")
+	}
+}
+func TestDeleteContact(t *testing.T) {
+	all.contacts[0] = Contact{"Barello", "Tomas", "53443344", 0}
+	all.DeleteContact(1)
+	if len(all.contacts) != 0 {
+		t.Error("Error! Can't be deleted")
+	}
+}
