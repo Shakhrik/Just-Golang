442012d705579df48731eb79994a01d107ecc229
Shakhriyor <ismatov.shaxriyor@mail.ru>
2020-11-13 11:28:30 +0500

    Modifed clent and server sides

--- grpc_learn/go.mod
+++ grpc_learn/go.mod
@@ -3,6 +3,7 @@ module github.com/Shakhrik/Just-Golang/grpc_learn
 go 1.15
 
 require (
+	github.com/gin-gonic/gin v1.6.3
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 )
--- grpc_learn/server/main.go
+++ grpc_learn/server/main.go
@@ -2,19 +2,34 @@ package main
 
 import (
 	"context"
-	"log"
+	"net"
 
 	"github.com/Shakhrik/Just-Golang/grpc_learn/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 type server struct{}
 
+func main() {
+	listener, err := net.Listen("tcp", ":4040")
+	if err != nil {
+		panic(err)
+	}
+	srv := grpc.NewServer()
+	proto.RegisterAddServiceServer(srv, &server{})
+	reflection.Register(srv)
+	if e := srv.Serve(listener); e != nil {
+		panic(err)
+	}
+}
 func (s *server) Add(ctx context.Context, request *proto.Request) (*proto.Response, error) {
 	a, b := request.GetA(), request.GetB()
-	log.Println(a, b)
-	return nil, nil
+	result := a + b
+	return &proto.Response{Result: result}, nil
 }
-
-func main() {
-
+func (s *server) Multiply(ctx context.Context, request *proto.Request) (*proto.Response, error) {
+	a, b := request.GetA(), request.GetB()
+	result := a * b
+	return &proto.Response{Result: result}, nil
 }
